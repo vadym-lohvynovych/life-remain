93915d72855fff85e2e7062dbb11f192bb602653
Vado <vadvoker@gmail.com>
2020-05-18 17:24:21 +0300

    purgecss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+const purgecssConfig = {
+  content: ['./src/index.html', './src/**/*.svelte'],
+  whitelistPatterns: [/svelte-/],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+};
+
 const plugins =
   process.env.NODE_ENV === 'production'
-    ? ['tailwindcss', 'autoprefixer', '@fullhuman/postcss-purgecss']
+    ? ['tailwindcss', 'autoprefixer', purgecss(purgecssConfig)]
     : ['tailwindcss'];
 
 module.exports = { plugins };

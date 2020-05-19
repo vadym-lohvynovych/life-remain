e5c5e33a4cac9c7af2476925ac6ace43ae58a8ef
Dmytro Petunenko <petunenkodv@gmail.com>
2020-05-19 23:31:04 +0200

    -- eslint for svelte3 added -- prettier config extended -- format existing files

--- src/stories/test.stories.js
+++ src/stories/test.stories.js
@@ -1,19 +1,19 @@
-import Test from './Test.svelte';
+import Test from './Test.svelte'
 
-import './tailwind.css';
+import './tailwind.css'
 
-export default { title: 'Test' };
+export default { title: 'Test' }
 
 export const Blue = () => ({
   Component: Test,
   props: {
     type: 'blue',
   },
-});
+})
 
 export const Red = () => ({
   Component: Test,
   props: {
     type: 'red',
   },
-});
+})

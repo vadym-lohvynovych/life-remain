50e4c884938fd9a677e8256955cf674c81d70a09
madvingovich <37845039+madvingovich@users.noreply.github.com>
2020-05-20 09:34:49 +0300

    Chore/clean dist before build, remove warnings (#4)
    * clean dist before build
    
    * remove default export in index
    
    * remove console warnings in storybook build, change package scripts

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,23 @@
 module.exports = async ({ config, mode }) => {
+  const isProd = mode === 'PRODUCTION';
+
+  const publicPath = config.output.path.includes('storybook')
+    ? '/storybook/'
+    : '/';
+
   config.output = {
     ...config.output,
-    publicPath: '/storybook/',
+    publicPath,
   };
+
+  config.performance = isProd
+    ? {
+        ...config.performance,
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+      }
+    : config.performance;
 
   return config;
 };

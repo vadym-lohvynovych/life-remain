3fa94c4fef893418c5bf5cbd7b4e3a6e76d2c2c1
Vado <vadvoker@gmail.com>
2020-05-21 17:54:36 +0300

    create simple datepicker storybook

---
+++ src/stories/datepicker/datepicker.stories.js
@@ -0,0 +1,24 @@
+
+import DatePicker from './DatePicker.svelte';
+
+import '../../styles/global.css';
+
+export default { title: 'Date Picker' };
+
+export const Default = () => ({
+  Component: DatePicker,
+  props: {
+    options: {
+      onChange: (selectedDates, dateStr, instance) => {
+        console.log('options change', dateStr)
+      },      
+      altInput: true,
+      maxDate: new Date()
+    }
+  },
+  on: {
+    change(e) {
+      console.log('on change', e)
+    }
+  }
+});

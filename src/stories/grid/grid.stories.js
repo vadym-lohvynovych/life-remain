bd30465ff8dea9d80d75b34d1af51f1b19d621d0
madvingovich <37845039+madvingovich@users.noreply.github.com>
2020-05-24 14:10:05 +0300

    Feature/datepicker storybook (#7)
    * create simple datepicker storybook
    
    * change viewport parameters
    
    * add flatpickr to purgecss whitelist
    
    * move stories imports and params to index file, add whitelistChildren to postcss
    
    * use flatpickr as action, add 'span' to purge witelist

--- src/stories/grid/grid.stories.js
+++ src/stories/grid/grid.stories.js
@@ -1,50 +1,6 @@
-import { addParameters } from '@storybook/svelte';
-
 import Grid from './Grid.svelte';
 
 import '../../styles/global.css';
-
-addParameters({
-  viewport: {
-    viewports: {
-      mobile: {
-        name: 'Mobile',
-        styles: {
-          width: '320px',
-          height: '568px',
-        },
-      },
-      tablet: {
-        name: 'Tablet',
-        styles: {
-          width: '768px',
-          height: '1024px',
-        },
-      },
-      laptop: {
-        name: 'Laptop',
-        styles: {
-          width: '1200px',
-          height: '1024px',
-        },
-      },
-      desktop: {
-        name: 'Desktop',
-        styles: {
-          width: '1366px',
-          height: '1024px',
-        },
-      },
-      fullHD: {
-        name: 'FullHD',
-        styles: {
-          width: '1920px',
-          height: '1080px',
-        },
-      },
-    },
-  },
-});
 
 export default { title: 'Grid' };
 

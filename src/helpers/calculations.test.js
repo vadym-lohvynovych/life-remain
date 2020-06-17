ba2ea3e767b57bdf0e33c4b10dbd8d137818fc69
Vado <vadvoker@gmail.com>
2020-06-17 17:57:08 +0300

    add 25 yeasr test

--- src/helpers/calculations.test.js
+++ src/helpers/calculations.test.js
@@ -1,11 +1,24 @@
 import { getLifeCount } from './calculations';
 
+const age = 25;
+const ageDate = new Date(Date.now - age * 1000 * 3600 * 24 * 365.25);
+const ageMonths = age * 12;
+const ageWeeks = Math.floor((age * 365.25) / 7);
+
 test('life count with current date returns correct years, months, weeks and lived values', () => {
   expect(getLifeCount(new Date())).toMatchObject({
     years: 90,
     months: 1080,
     weeks: 4696,
     lived: 0
+  });
+});
+
+test('life count returns correct object with 25 years date', () => {
+  expect(getLifeCount(ageDate, age)).toMatchObject({
+    years: age,
+    months: ageMonths,
+    weeks: ageWeeks
   });
 });
 
@@ -27,4 +40,5 @@ test('life count throwing Error if date is in the future', () => {
     error = e;
   }
   expect(error instanceof Error).toBe(true);
+  // expect(getLifeCount(new Date(Date.now() + 1))).toThrowError();
 });

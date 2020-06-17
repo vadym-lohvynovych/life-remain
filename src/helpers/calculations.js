c8a27f5375ab28519902b5db7a11213252dab114
Vado <vadvoker@gmail.com>
2020-06-17 13:19:03 +0300

    add tests for getLifeCount

--- src/helpers/calculations.js
+++ src/helpers/calculations.js
@@ -1,16 +1,24 @@
 export function getLifeCount(date, maxAge = 90) {
+  if (!(date instanceof Date))
+    throw new TypeError('You should specify a date object in first parameter');
+
   const toDays = 1000 * 3600 * 24;
+
   const ageMiliseconds = Date.now() - date.getTime();
   const lifeMiliseconds = maxAge * 365.25 * toDays;
 
   const weeks = Math.floor(lifeMiliseconds / (toDays * 7));
   const months = Math.floor(lifeMiliseconds / (toDays * (365.25 / 12)));
-  const years = Math.floor(lifeMiliseconds / (toDays * 365.25));
+  const lived = (ageMiliseconds / lifeMiliseconds) * 100;
+
+  if (lived < 0) {
+    throw new Error('Seems like you have a time machine');
+  }
 
   return {
     weeks,
     months,
-    years,
+    years: maxAge,
     lived: (ageMiliseconds / lifeMiliseconds) * 100
   };
 }
